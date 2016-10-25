e7a555aa5af3b4995280424c841a72093d3fa20f
Jacob Hoving <jacob_hoving@hotmail.com>
2016-10-25 18:09:19 -0400

    Save screen. And tries to save screenshot image using Cordova, but not working.

---
+++ NOTES.md
@@ -0,0 +1,9 @@
+    meteor add cordova:cordova-screenshot@https://github.com/gitawego/cordova-screenshot/tarball/39bc1dff938c6f31684929c9308ea995d2f813de
+
+    // From https://github.com/gitawego/cordova-screenshot/issues/95 to avoid blocking error, but doesn't seem to help
+    - (void)myPluginMethod:(CDVInvokedUrlCommand*)command { // Check command.arguments here.
+        [self.commandDelegate runInBackground:^{ NSString* payload = nil; // Some blocking logic...
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload]; // The sendPluginResult method is thread-safe.
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }];
+    }
--- client/main.js
+++ client/main.js
@@ -3,7 +3,7 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-var screens = ['space', 'actor', 'force'];
+var screens = ['space', 'actor', 'force', 'save'];
 var num_images = [6, 6, 6];
 
 Template.body.onCreated(function helloOnCreated() {
@@ -38,53 +38,73 @@ Template.body.events({
     // Update the current screen number. Footer will update automatically because
     // screen_num is a reactive variable.
     Session.set('screen_num', Session.get('screen_num') + 1);
-
-    // Set a random image for this screen
     var screen_name = screens[Session.get('screen_num')];
-    var max_image = num_images[Session.get('screen_num')];
-    Session.set(screen_name + '_image', Math.floor(Math.random() * max_image) + 1);
 
-    // Make it draggable
-    interact(`.${screen_name}`)
-      .draggable({
-        // enable inertial throwing
-        inertia: true,
-        // call this function on every dragmove event
-        onmove: dragMoveListener,
-        // call this function on every dragend event
-        onend: function (event) { }
-      });
-
-    function dragMoveListener (event) {
-      var target = $(event.target).find('.translate');
-      var x = parseFloat(target.data('x') || 0) + event.dx;
-      var y = parseFloat(target.data('y') || 0) + event.dy;
-
-      // translate the element
-      var transform = 'translate(' + x + 'px, ' + y + 'px)';
-      target.css({webkitTransform: transform, transform: transform});
-
-      // update the posiion attributes
-      target.data({x: x, y: y});
+    if (screen_name == 'save') {
+      save_screenshot();
     }
-
-    window.dragMoveListener = dragMoveListener;
-
-    // Make it rotatable
-    interact(`.${screen_name}`).gesturable({
-      onmove: function (event) {
-        var target = $(`.${screen_name}`).find('.rotate');
-        var angle = parseFloat(target.data('angle') || 0) + event.da;
-
-        var transform = 'rotate(' + angle + 'deg)';
-        target.css({webkitTransform: transform, transform: transform});
-
-        target.data('angle', angle);
-      }
-    });
+    else {
+      spawn_image();
+    }
 
     // Show the new screen
     $(`.${screen_name}`).show();
+
+    function spawn_image() {
+      // Set a random image for this screen
+      var max_image = num_images[Session.get('screen_num')];
+      Session.set(screen_name + '_image', Math.floor(Math.random() * max_image) + 1);
+
+      // Make it draggable
+      interact(`.${screen_name}`)
+        .draggable({
+          // enable inertial throwing
+          inertia: true,
+          // call this function on every dragmove event
+          onmove: dragMoveListener,
+          // call this function on every dragend event
+          onend: function (event) { }
+        });
+
+      function dragMoveListener (event) {
+        var target = $(event.target).find('.translate');
+        var x = parseFloat(target.data('x') || 0) + event.dx;
+        var y = parseFloat(target.data('y') || 0) + event.dy;
+
+        // translate the element
+        var transform = 'translate(' + x + 'px, ' + y + 'px)';
+        target.css({webkitTransform: transform, transform: transform});
+
+        // update the posiion attributes
+        target.data({x: x, y: y});
+      }
+
+      window.dragMoveListener = dragMoveListener;
+
+      // Make it rotatable
+      interact(`.${screen_name}`).gesturable({
+        onmove: function (event) {
+          var target = $(`.${screen_name}`).find('.rotate');
+          var angle = parseFloat(target.data('angle') || 0) + event.da;
+
+          var transform = 'rotate(' + angle + 'deg)';
+          target.css({webkitTransform: transform, transform: transform});
+
+          target.data('angle', angle);
+        }
+      });
+    }
+
+    function save_screenshot() {
+      console.log('Saving screenshot');
+      navigator.screenshot.save(function(error,res){
+        if (error) {
+          console.error(error);
+        } else {
+          console.log('Saved screenshot', res.filePath);
+        }
+      });
+    }
 
   },
 });
